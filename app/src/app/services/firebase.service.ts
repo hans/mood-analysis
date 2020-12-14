8ab1db904d8c80b2b7aea55eda3f1a68fbdd35de
Jon Gauthier <jon@gauthiers.net>
2020-12-14 12:22:32 -0500

    When adding entries, also auto-add activities if missing, and track activity counts

--- app/src/app/services/firebase.service.ts
+++ app/src/app/services/firebase.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { AngularFirestore, AngularFirestoreDocument, DocumentChangeAction, DocumentSnapshot } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument, DocumentChangeAction, DocumentReference, DocumentSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-
 export interface Emotion { id?: string, name: string };
-export interface Activity { id?: string, name: string };
+export interface Activity { id?: string, count: number };
 
 
 @Injectable({
@@ -44,7 +43,40 @@ export class FirebaseService {
     entry.createdAt = new Date();
     console.log(entry);
 
-    return this.db.collection("entries").add(entry);
+    // TODO run this in a transaction?
+    // Get documents for each activity
+    entry.activities.forEach((activity: string) => {
+      var activityDoc: AngularFirestoreDocument<Activity> =
+        this.db.collection("activities").doc(activity);
+      activityDoc.get().subscribe(doc => {
+        var count = doc.exists ? doc.data().count || 0 : 0;
+        activityDoc.set({count: count + 1}, {merge: true});
+      })
+    });
+    // })
+    // // var activityRefs = entry.activities.map(
+    // //   (activity: string) => this.db.collection("activities").doc(activity));
+    //
+    //
+    // this.db.firestore.runTransaction(async t => {
+    //   // For each activity, update corresponding counts
+    //   activityRefs.map((ref: DocumentReference<Activity>) => {
+    //     t.get(ref).then(activityDoc => {
+    //       var method = t.set, count = 0;
+    //       if (activityDoc.exists) {
+    //         method = t.update;
+    //         count = activityDoc.data().count;
+    //       }
+    //
+    //       console.log(ref);
+    //       method(ref, {count: count + 1})
+    //     })
+    //   });
+    //
+    //   t.set(doc, entry);
+    // });
+
+    this.db.collection("entries").add(entry);
   }
 
 }
