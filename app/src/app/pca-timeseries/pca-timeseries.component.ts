36a8e56394310428737ec47e641cad1712db41a2
Jon Gauthier <jon@gauthiers.net>
2021-01-03 23:06:18 -0500

    Compute time-windowed moving averages for PCA timeseries

--- app/src/app/pca-timeseries/pca-timeseries.component.ts
+++ app/src/app/pca-timeseries/pca-timeseries.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { Chart, ChartDataSets, ChartOptions, TimeUnit } from 'chart.js';
+import {
+  Chart,
+  ChartPoint,
+  ChartDataSets,
+  ChartOptions,
+  TimeUnit,
+} from 'chart.js';
 import * as pluginZoom from 'chartjs-plugin-zoom';
 import { Color } from 'ng2-charts';
 
@@ -79,7 +85,7 @@ export class PcaTimeseriesComponent implements OnInit {
       this.record.projectedData,
     ).to2DArray();
 
-    const datasets = _.range(this.numComponents).map((idx) => {
+    const datasets: ChartDataSets[] = _.range(this.numComponents).map((idx) => {
       const componentData = _.zip(this.entries, pcaData).map((el) => {
         const [entry, vector] = el;
         return { x: (<any>entry.createdAt).toDate(), y: vector[idx] };
@@ -93,6 +99,54 @@ export class PcaTimeseriesComponent implements OnInit {
       return dataset;
     });
 
-    return datasets;
+    // Add moving average datasets.
+    // TODO extract magic number
+    const movingAverageDatasets = datasets.map(
+      (dataset) => this.computeMovingAverage(dataset, 1000 * 60 * 60 * 12), // 12 hour step size
+    );
+
+    return datasets.concat(movingAverageDatasets);
+  }
+
+  /**
+   * Compute a moving average of the given dataset over the given time window.
+   * Data points are resampled at the time window / frequency specified by
+   * `window` in millisecond units.
+   */
+  computeMovingAverage(
+    dataset: ChartDataSets,
+    stepSize: number,
+  ): ChartDataSets {
+    const data = _.orderBy(dataset.data as ChartPoint[], (d) => d.x),
+      dates = data.map((d) => d.x as Date),
+      minDate = dates[0],
+      maxDate = dates[dates.length - 1],
+      minTime = minDate.getTime();
+
+    const steps = _.range(minDate.getTime(), maxDate.getTime(), stepSize),
+      buckets: { [bucket: number]: number[] } = _.fromPairs(
+        steps.map((s) => [s, []]),
+      );
+    data.forEach((d) => {
+      const bucket =
+        Math.floor(((<Date>d.x).getTime() - minTime) / stepSize) * stepSize +
+        minTime;
+      buckets[bucket].push(<number>d.y);
+    });
+
+    const avgData = Object.entries(buckets).map((el) => {
+      const [bucket, values] = el;
+      return {
+        x: new Date(Number(bucket)),
+        y: _.mean(values),
+      };
+    });
+
+    const avgDataset = {
+      label: `${dataset.label} moving average`,
+      data: avgData as ChartPoint[],
+    };
+
+    return avgDataset;
   }
 }
