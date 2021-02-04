1ead87edf9c6e89c1e3f61540b3815cd8fc81d68
Jon Gauthier <jon@gauthiers.net>
2021-02-04 11:18:08 -0500

    Working authentication; guard form page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
 
 import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
@@ -7,7 +8,11 @@ import { StatsComponent } from './stats/stats.component';
 import { StatsDetailsComponent } from './stats-details/stats-details.component';
 
 const routes: Routes = [
-  { path: 'form', component: FormComponent },
+  {
+    path: 'form',
+    component: FormComponent,
+    canActivate: [AngularFireAuthGuard],
+  },
   { path: 'list', component: ListComponent },
   { path: 'stats', component: StatsComponent },
   { path: 'stats/:id', component: StatsDetailsComponent },
--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -11,12 +11,15 @@ import firebase from 'firebase/app';
 export class AppComponent {
   title = 'app';
 
-  constructor(public auth: AngularFireAuth) {
-    this.auth.currentUser.then((x) => console.log("current user", x));
-  }
+  constructor(public auth: AngularFireAuth) {}
 
   login() {
-    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((c) => console.log(c));
+    this.auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((c) => console.log(c));
   }
 
+  logout() {
+    this.auth.signOut();
+  }
 }
